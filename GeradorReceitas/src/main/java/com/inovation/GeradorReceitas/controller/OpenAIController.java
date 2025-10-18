0cfe57052bf8eca8dac470b6591cc8279fd93761
arnaldo felipe <feelipesilvaa11@gmail.com>
2025-10-17 22:33:30 -0300

    criando meu arquivo dokcerfiler para futuramente subir meu front com reacte e backend com api num docker compose

--- GeradorReceitas/src/main/java/com/inovation/GeradorReceitas/controller/OpenAIController.java
+++ GeradorReceitas/src/main/java/com/inovation/GeradorReceitas/controller/OpenAIController.java
@@ -2,6 +2,7 @@ package com.inovation.GeradorReceitas.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import com.inovation.GeradorReceitas.service.ReceitaService;
 @RestControllerAdvice
 @RestController
 @RequestMapping("/receita")
+@CrossOrigin(origins = "http://localhost:5173")
 public class OpenAIController {
 
 
